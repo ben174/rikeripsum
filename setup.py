44c213476c905b2dd08a8c5152bca5d67c606b41
Ben Friedland <ben174@gmail.com>
2013-06-22 18:31:11 -0700

    Basic project packaging.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='RikerIpsum',
+    version='1.0',
+    packages=['rikeripsum',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
