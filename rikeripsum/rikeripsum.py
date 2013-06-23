c29f3b4beb1da0b0f26f7a23b20ffc795679beb2
Ben Friedland <ben174@gmail.com>
2013-06-22 18:34:05 -0700

    Option to choose number of "sentences" to generate.

--- rikeripsum/rikeripsum.py
+++ rikeripsum/rikeripsum.py
@@ -10,11 +10,12 @@ def main():
     print generate_paragraph()
 
 
-def generate_paragraph(): 
+def generate_paragraph(sentence_count=None): 
     global lines
     if not lines: 
         lines = load_pickle()
-    sentence_count = random.choice(range(2, 10))
+    if not sentence_count:
+        sentence_count = random.choice(range(2, 10))
     paragraph = ''
     for i in range(sentence_count): 
         paragraph += ' ' + random.choice(lines)['text']
