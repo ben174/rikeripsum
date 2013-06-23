00a277fe87bdc9ec4363090a36f664b6dc3314f3
Ben Friedland <ben174@gmail.com>
2013-06-22 18:28:40 -0700

    Singleton-type pickle.

--- rikeripsum/rikeripsum.py
+++ rikeripsum/rikeripsum.py
@@ -5,20 +5,21 @@ import random
 
 lines = None
 
+
 def main(): 
-    global lines
-    lines = load_pickle()
     print generate_paragraph()
 
 
 def generate_paragraph(): 
+    global lines
+    if not lines: 
+        lines = load_pickle()
     sentence_count = random.choice(range(2, 10))
     paragraph = ''
     for i in range(sentence_count): 
         paragraph += ' ' + random.choice(lines)['text']
     return paragraph.strip()
     
-
 
 def load_pickle(): 
     f = open('riker.pickle')
