948e7dc14b2eadb339edf3fbad63376782642d65
Ben Friedland <ben174@gmail.com>
2013-06-23 11:31:35 -0700

    Version 1.1. Added documentation and some methods to specified desired numbers of sentences/words.

--- rikeripsum/rikeripsum.py
+++ rikeripsum/rikeripsum.py
@@ -6,8 +6,8 @@ import os
 
 lines = None
 
-
 _ROOT = os.path.abspath(os.path.dirname(__file__))
+
 def get_data(path):
     return os.path.join(_ROOT, 'data', path)
 
@@ -17,20 +17,63 @@ def main():
 
 
 def generate_paragraph(sentence_count=None): 
-    global lines
-    if not lines: 
-        lines = load_pickle()
+    """Generates a 'paragraph' consisting of sentence_count 'sentences'.
+    If sentence_count is not provided, a random number between two and
+    ten will be chosen.
+    
+    """
     if not sentence_count:
         sentence_count = random.choice(range(2, 10))
     paragraph = ''
     for i in range(sentence_count): 
         paragraph += ' ' + random.choice(lines)['text']
     return paragraph.strip()
-    
+
+
+def generate_sentence(word_count=None): 
+    """Returns a 'sentence'. A sentence is actually one line of dialog
+    by William Riker, and may in fact consiste of multiple sentences.
+    If a word_count is provided, the generator will attempt to return 
+    a sentence with that number of words. Or come as close as possible. 
+    Note that higher numbers of word will become increasingly unique to
+    the distribution and may result in a less 'random' sentence. 
+
+    """
+    global lines
+    if not lines: 
+        lines = load_pickle()
+    if not word_count: 
+        return random.choice(lines)['text']
+
+    potential_matches = [line for line in lines if 
+        line['word_count'] == word_count]
+    if potential_matches: 
+        return random.choice(potential_matches)['text']
+    else: 
+        if word_count == 1:
+            raise ImpossibleSentenceError('Couldn\'t generate a sentence with \
+                the requested number of words.')
+        # recursive callback, trying one less words each time. 
+        return generate_sentence(word_count - 1)
+
 
 def load_pickle(): 
+    """Loads up sentence data. All methods in this class which use 
+    phrase data should call this if global lines == None.
+
+    """
     f = open(get_data('riker.pickle'))
     return pickle.load(f)
+
+
+class ImpossibleSentenceError(Exception):
+    """Called when the engine is unable to fufill the request due to lack 
+    of potential data. This would usually be raised if a number of sentences 
+    was requested which the engine did not have data to fulfill.
+
+    """
+    def __init__(self, message, Errors):
+        Exception.__init__(self, message)
 
 
 if __name__ == '__main__': 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='RikerIpsum',
-    version='1.0',
+    version='1.1',
     packages=['rikeripsum',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description='''Generates text - like lorem ipsum - but uses real English. Taken from random samplings of dialog spoken by Commander William Riker in Star Trek: The Next Generation.
@@ -12,8 +12,7 @@ setup(
     @jessicaspacekat (https://twitter.com/jessicaspacekat) came up with the brilliant idea of using Riker's dialog for generating text. She created a fancy javascript generator, but I wanted something I could use in my projects - for provisioning sample data to test with. I also wanted to use all of Riker's dialog, rather than a limited subset.
     
     I found a site, http://antoa.com/tng/, which contains the scripts of every episode of Star Trek: The Next Generation. I scraped the site then parsed out all Riker's lines. After a little bit of cleaning up, I created a large object full of every line he's ever spoken in TNG. I saved that to a pickle for quick loading, and wrote rikeripsum.py to generate random sentences.
-    
-    TODO - Option to specify the number of words you'd like sentences to have. - Ability to select which character you'd like to pull lines from (this is started). - Perhaps a way to construct truly random sentences using a sampling of words from the dialog.''',
+    ''',  
     data_files=[('data', ['rikeripsum/data/riker.pickle']),], 
     package_data = {
         '': ['*.pickle', 'data/riker.pickle' ],
